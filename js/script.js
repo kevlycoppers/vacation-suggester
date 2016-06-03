0f6a209dbb603533e87bcf200b0e2dc69693e651
Levi Hopkins <kevlycoppers@gmail.com>
2016-06-03 14:36:19 -0700

    remove carousel and add in links to websites dedicated to destination

--- js/script.js
+++ js/script.js
@@ -5,22 +5,35 @@ $(document).ready(function() {
     var activities = $("#activities").val();
     var food = $("#food").val();
     var self = $("#self").val();
-    var vic;
+    var vic1;
+    var vic2;
+    var vic3;
+    var vic4;
+
 
     if (vacation === "Relaxed and lazy" && (zone === "Tropical" || zone === "Neither") && (activities === "Hiking" || activities === "Boating" || activities === "Swimming") && (food === "Asian" || food === "Vegan" || food === "American") && (self === "Shy" || self === "Polite" || self === "Upbeat")) {
-      vic = "Fiji";
+      vic1 = "Fiji";
     }
     else if (vacation === "Busy and full of activity"  && (zone === "Tropical" || zone === "Tundra" || zone === "Neither") && activities === "Racing" && food === "American" && (self === "Upbeat" || self === "Polite" || self === "Outgoing")) {
-      vic = "Virginia International Raceway"
+      vic2 = "Virginia International Raceway"
     }
     else if (vacation === "Free and adventurous" && zone === "Tundra" && (activities === "Hiking" || activities === "Swimming") && food === "European" && (self === "Shy" || self === "Polite" || self === "Outgoing")) {
-      vic = "The Island of Iceland"
+      vic3 = "The Island of Iceland"
     }
     else {
-      vic = "The Island of Hawaii"
+      vic4 = "The Island of Hawaii"
     }
-    $("#match").show();
-    $(".vic").text(vic)
+    $("#match1").show();
+    $(".vic1").text(vic1)
+
+    $("#match2").show();
+    $(".vic2").text(vic2)
+
+    $("#match3").show();
+    $(".vic3").text(vic3)
+
+    $("#match4").show();
+    $(".vic4").text(vic4)
     event.preventDefault();
   });
 });
